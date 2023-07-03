f088c7e9064c070d32905018caff43b8f083f1be
Sahil0718 <135214197+Sahil0718@users.noreply.github.com>
2023-07-03 21:05:58 +0545

    CREATING A RED SQUARE WHEN MOUSE GETS CLICKED v1.0.cpp

--- CREATING A RED SQUARE WHEN MOUSE GETS CLICKED v1.0.cpp
+++ CREATING A RED SQUARE WHEN MOUSE GETS CLICKED v1.0.cpp
@@ -1,15 +1,12 @@
 #include <GL/glut.h>
 
 void draw(int button , int state, int x, int y){
-glClearColor(1,1,1,1);
-glClear(GL_COLOR_BUFFER_BIT);
-
 int width = 100;
 int height = 50;
 
 glLineWidth(3);
 glBegin(GL_QUADS);
-glColor3f(0.5,1,0.5);
+glColor3d(1,0,0);
 glVertex2i(x,y);
 glVertex2i(x+width, y);
 glVertex2i(x+width , y+height);
@@ -19,18 +16,25 @@ glEnd();
 glFlush();
 
 }
+void Display(){
+glClearColor(1,1,1,1); //background color set to white
+glClear(GL_COLOR_BUFFER_BIT);
+glFlush();
+}
+
 
 void myInit(){
     glutInitWindowSize(800,600);
     glutInitWindowPosition(50,50);
     glutInitDisplayMode(GLUT_RGB);
     glutCreateWindow("CREATING A BOX ON MOUSE CLICK!");
+    glutDisplayFunc(Display);
     glClearColor(1,1,1,1);
     glClear(GL_COLOR_BUFFER_BIT);
     glColor3d(0,0,0);
     glMatrixMode(GL_PROJECTION);
     glLoadIdentity();
-    gluOrtho2D(0,600,0,500);
+    gluOrtho2D(0,800,0,400);
     glMatrixMode(GL_MODELVIEW);
 }
 
@@ -41,5 +45,4 @@ int main(int argc , char *argv[]){
     glutMainLoop();
 
 }
-
 
